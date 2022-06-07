d4a8600cf28e3ef45450c33cbe2b4801bd28a1f3
p-vascool <vasko_pan1@mail.bg>
2022-06-07 23:52:10 +0300

    Added Chain of responsibility

---
+++ DesignPatterns.Behavioral/ChainOfResponsibility/AbstractHandler.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatterns.Behavioral.ChainOfResponsibility
+{
+    public class AbstractHandler : IHandler
+    {
+        private IHandler _nextHandler;
+        public virtual object Handle(object request)
+        {
+            if (this._nextHandler != null)
+            {
+                return this._nextHandler.Handle(request);
+            }
+            else
+            {
+                return null;
+            }    
+        }
+
+        public IHandler SetNext(IHandler next)
+        {
+            this._nextHandler = next;
+
+            return this._nextHandler;
+        }
+    }
+}
--- DesignPatterns.Behavioral/Program.cs
+++ DesignPatterns.Behavioral/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using DesignPatterns.Behavioral.ChainOfResponsibility;
+
+#region ChainOfResposibility
+
+var monkey = new MonkeyHandler();
+var squirel = new SquirrelHandler();
+var dog = new DogHandler();
+
+monkey
+    .SetNext(squirel)
+    .SetNext(dog);
+
+Console.WriteLine("Chain: Monkey > Squirrel > Dog\n");
+CORClient.ClientCode(monkey);
+Console.WriteLine();
+
+Console.WriteLine("Subchain: Squirrel > Dog\n");
+CORClient.ClientCode(squirel);
+#endregion
