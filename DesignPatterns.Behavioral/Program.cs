85d6cb8979c479af95fb9075e2e15f356bc183f3
p-vascool <vasko_pan1@mail.bg>
2022-06-08 23:18:36 +0300

    Added mediator pattern

--- DesignPatterns.Behavioral/Program.cs
+++ DesignPatterns.Behavioral/Program.cs
@@ -1,6 +1,7 @@
 ﻿using DesignPatterns.Behavioral.ChainOfResponsibility;
 using DesignPatterns.Behavioral.Command;
 using DesignPatterns.Behavioral.Iterator;
+using DesignPatterns.Behavioral.Mediator;
 
 #region ChainOfResposibility
 
@@ -53,4 +54,23 @@ foreach (var element in collection)
 {
     Console.WriteLine(element);
 }
+#endregion
+
+#region Mediator
+
+var component1 = new Component1();
+var component2 = new Component2();
+new ConcreteMediator(component1, component2);
+
+Console.WriteLine();
+Console.WriteLine();
+
+
+Console.WriteLine("Client triggets operation A.");
+component1.DoA();
+
+Console.WriteLine();
+
+Console.WriteLine("Client triggers operation D.");
+component2.DoD();
 #endregion
